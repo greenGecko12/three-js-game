38cf258209461ff67b9d4fc7df5f0bae7cb49e2a
Sai Kommuru <sai.kommuru123@gmail.com>
2023-01-11 21:08:08 +0000

    removed all objects and changed colour

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { PointerLockControls } from './node_modules/three/examples/jsm/controls/
 
 let camera, scene, renderer, controls;
 
-const objects = [];
+// const objects = [];
 
 let raycaster;
 
@@ -16,11 +16,11 @@ let canJump = false;
 let prevTime = performance.now();
 const velocity = new THREE.Vector3();
 const direction = new THREE.Vector3();
-const vertex = new THREE.Vector3();
-const color = new THREE.Color();
+// const vertex = new THREE.Vector3();
+// const color = new THREE.Color();
 
 init();
-animate();
+animate(); // animation loop
 
 function init() {
 
@@ -63,6 +63,7 @@ function init() {
 
     scene.add(controls.getObject());
 
+    // moving the player around
     const onKeyDown = function (event) {
 
         switch (event.code) {
@@ -124,89 +125,66 @@ function init() {
 
     };
 
+    // Adding in the event listeners to call the movement
     document.addEventListener('keydown', onKeyDown);
     document.addEventListener('keyup', onKeyUp);
 
-    // TODO: what is a THREE.Raycaster
+    // what is a THREE.Raycaster --> lighting and shading basically
     raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, - 1, 0), 0, 10);
 
-    // floor
-    let floorGeometry = new THREE.PlaneGeometry(2000, 2000, 100, 100);
+    // floor - The first 2 arguments (width, height)
+    // The last 2 arguments correspond to the number of width and height segments --> affect the design on the floor
+    let floorGeometry = new THREE.PlaneGeometry(200, 200);
+    
+    // makes the plane a proper plane (otherwise it defaults to a vertical plane)
     floorGeometry.rotateX(- Math.PI / 2);
 
     // vertex displacement
-    let position = floorGeometry.attributes.position;
+    // let position = floorGeometry.attributes.position;
+    // console.log(position);
+    // console.log(typeof(position))
 
-    for (let i = 0, l = position.count; i < l; i++) {
+    // this next chunk "designs" the floor I think
+    /////////////////////////////////////////////////////////////////////////////////////
+    // for (let i = 0, l = position.count; i < l; i++) {w 
 
-        vertex.fromBufferAttribute(position, i);
+    //     vertex.fromBufferAttribute(position, i);
 
-        vertex.x += Math.random() * 20 - 10;
-        vertex.y += Math.random() * 2;
-        vertex.z += Math.random() * 20 - 10;
+    //     vertex.x += Math.random() * 20 - 10;
+    //     vertex.y += Math.random() * 2;
+    //     vertex.z += Math.random() * 20 - 10;
 
-        position.setXYZ(i, vertex.x, vertex.y, vertex.z);
+    //     position.setXYZ(i, vertex.x, vertex.y, vertex.z);
 
-    }
+    // }
 
-    floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
+    // floorGeometry = floorGeometry.toNonIndexed(); // ensure each face has unique vertices
 
-    position = floorGeometry.attributes.position;
-    const colorsFloor = [];
+    // position = floorGeometry.attributes.position;
+    // const colorsFloor = [];
 
-    for (let i = 0, l = position.count; i < l; i++) {
+    // for (let i = 0, l = position.count; i < l; i++) {
 
-        color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-        colorsFloor.push(color.r, color.g, color.b);
+    //     color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+    //     colorsFloor.push(color.r, color.g, color.b);
 
-    }
+    // }
 
-    floorGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsFloor, 3));
+    // floorGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsFloor, 3));
 
-    const floorMaterial = new THREE.MeshBasicMaterial({ vertexColors: true });
+    const floorMaterial = new THREE.MeshLambertMaterial({ color: 0x23da23 });
 
     const floor = new THREE.Mesh(floorGeometry, floorMaterial);
     scene.add(floor);
 
-    // objects
+    //////////////////////////////////////////////////////////////////////////////////////
 
-    const boxGeometry = new THREE.BoxGeometry(20, 20, 20).toNonIndexed();
-
-    position = boxGeometry.attributes.position;
-    const colorsBox = [];
-
-    for (let i = 0, l = position.count; i < l; i++) {
-
-        color.setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-        colorsBox.push(color.r, color.g, color.b);
-
-    }
-
-    boxGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colorsBox, 3));
-
-    for (let i = 0; i < 500; i++) {
-
-        const boxMaterial = new THREE.MeshPhongMaterial({ specular: 0xffffff, flatShading: true, vertexColors: true });
-        boxMaterial.color.setHSL(Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-
-        const box = new THREE.Mesh(boxGeometry, boxMaterial);
-        box.position.x = Math.floor(Math.random() * 20 - 10) * 20;
-        box.position.y = Math.floor(Math.random() * 20) * 20 + 10;
-        box.position.z = Math.floor(Math.random() * 20 - 10) * 20;
-
-        scene.add(box);
-        objects.push(box);
-
-    }
-
-    //
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    //
 
     window.addEventListener('resize', onWindowResize);
 
@@ -221,6 +199,7 @@ function onWindowResize() {
 
 }
 
+// animation function that gets called 60 times a second, I think
 function animate() {
 
     requestAnimationFrame(animate);
@@ -232,9 +211,9 @@ function animate() {
         raycaster.ray.origin.copy(controls.getObject().position);
         raycaster.ray.origin.y -= 10;
 
-        const intersections = raycaster.intersectObjects(objects, false);
+        // const intersections = raycaster.intersectObjects(objects, false);
 
-        const onObject = intersections.length > 0;
+        // const onObject = intersections.length > 0;
 
         const delta = (time - prevTime) / 1000;
 
@@ -250,12 +229,12 @@ function animate() {
         if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
         if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
 
-        if (onObject === true) {
+        // if (onObject === true) {
 
-            velocity.y = Math.max(0, velocity.y);
-            canJump = true;
+        //     velocity.y = Math.max(0, velocity.y);
+        //     canJump = true;
 
-        }
+        // }
 
         controls.moveRight(- velocity.x * delta);
         controls.moveForward(- velocity.z * delta);
